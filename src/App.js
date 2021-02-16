60bb00a22a7919b6fb0221e82c50d2caf698a3a6
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-16 21:32:22 +0200

    first commit, npm install and basic structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+
+
+  return (
+    <section className='section-center'>
+      <h3>Tired of boring lorem ipsum?</h3>
+      <form className='lorem-form'>
+
+      </form>
+    </section>
+  )
+}
+
+export default App;
